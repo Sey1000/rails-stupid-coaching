9105137cd497141a44b6b402a088524a12c77927
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-14 11:39:22 +0200

    Make it pretty

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,16 @@
 class QuestionsController < ApplicationController
   def answer
-    @query = params[:query]
+    @message = params[:query]
+    answer = ""
+    if @message == @message.upcase
+      answer << "I can feel your motivation! "        
+    end
+    if @message[-1] == "?"
+      answer << "Silly question, get dressed and go to work!"
+    else
+      answer << "I don't care, get dressed and go to work!"
+    end
+    @coach_answer = answer
   end
 
   def ask
